f4ebbf1080a320d8ad8f3d188af0568f98776d26
aleks1303 <aleksbulda13@gmail.com>
2025-04-07 19:17:42 +0300

    services

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,7 +3,6 @@ import {IUser} from "../models/IUser.ts";
 const getUsers = async (): Promise<IUser[]> => {
   return  await fetch('https://jsonplaceholder.typicode.com/users')
         .then((response) => response.json())
-
 }
 export {
     getUsers
