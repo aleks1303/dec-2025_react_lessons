1e5df8e692f4c84754ac8b33b12cef5dc7fe3bfd
aleks1303 <aleksbulda13@gmail.com>
2025-04-17 14:55:40 +0300

    master

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {router} from "./routes/routes.tsx";
-import {RouterProvider} from "react-router-dom";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router}/>)
+createRoot(document.getElementById('root')!).render(<App/>)
