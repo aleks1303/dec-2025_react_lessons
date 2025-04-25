a07465098dd3493965067cd2cbe8b089bb968516
aleks1303 <aleksbulda13@gmail.com>
2025-04-25 15:08:48 +0300

    homework task-1

--- src/components/form-components/FormComponent.tsx
+++ src/components/form-components/FormComponent.tsx
@@ -1,11 +1,14 @@
 import {useForm} from "react-hook-form";
 import {ICar} from "../../models/ICar.ts";
 import {addCar} from "../../services/api.service.tsx";
+import {joiResolver} from "@hookform/resolvers/joi";
+import {useValidator} from "../../validator/CarValidator.ts";
 
 
 const FormComponent = () => {
-    const {register, handleSubmit} = useForm<ICar>({
-        mode:'all'
+    const {register, handleSubmit, formState: {errors}} = useForm<ICar>({
+        mode:'all',
+        resolver: joiResolver(useValidator)
     });
     const createCar = (formData: ICar) => {
        addCar(formData)
@@ -18,14 +21,17 @@ const FormComponent = () => {
             <form onSubmit={handleSubmit(createCar)}>
                 <label>
                     <input className={baseClassInput} type="text" {...register('brand')} placeholder={'brand'}/>
+                    {errors.brand && <div>{errors.brand.message}</div>}
                 </label>
 
                 <label>
                     <input className={baseClassInput} type="number" {...register('price')} placeholder={'price'}/>
+                    {errors.price && <div>{errors.price.message}</div>}
                 </label>
 
                 <label>
                     <input className={baseClassInput} type="number"{...register('year')} placeholder={'year'}/>
+                    {errors.year && <div>{errors.year.message}</div>}
                 </label>
                 <button className={baseClassButton}>sent</button>
             </form>
--- src/validator/CarValidator.ts
+++ src/validator/CarValidator.ts
@@ -1,2 +1,32 @@
+import Joi from "joi";
 
-const useValidator =
+const useValidator = Joi.object({
+    brand: Joi.string()
+        .pattern( new RegExp('^[a-zA-Zа-яА-яёЁіІїЇєЄҐґ]{1,20}$')).required()
+        .min(1).required()
+        .max(20).required()
+        .messages({
+            'string.pattern.base': "Does not match the expectation",
+            "string.min":"Minimum length 1",
+            "string.max":"Maximum length 20"
+        }),
+    price: Joi.number()
+        .min(0)
+        .max(1000000)
+        .messages({
+            "number.min": "The price is lower than expected",
+            "number.max": "The price is lower than expected"
+        }),
+
+    year: Joi.number()
+        .min(1990)
+        .max(2024)
+        .messages({
+            "number.min": "The year is smaller than expected",
+            "number.max": "The year is bigger than expected"
+        })
+})
+
+export {
+    useValidator
+}
