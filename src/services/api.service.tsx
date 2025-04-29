c1d6e7c84bc6361f37ca5ed73ed8b9f657eeec9d
aleks1303 <aleksbulda13@gmail.com>
2025-04-29 20:27:57 +0300

    authentication

--- src/services/api.service.tsx
+++ src/services/api.service.tsx
@@ -1,27 +1,64 @@
 import axios from "axios";
-import {IUserWithToken} from "../models/IUserWithTokens.ts";
+import {IUserWithTokens} from "../models/IUserWithTokens.ts";
+import {IProduct} from "../models/IProduct.ts";
+import {IProductResponseType} from "../models/IProductResponseType.ts";
+import {getLocalStorage} from "./helpers.tsx";
+import {ITokensPair} from "../models/ITokensPair.ts";
 
+
+type LoginData = {
+    username:string,
+    password:string,
+    expiresInMins: number
+}
 
 const axiosInstance = axios.create({
     baseURL: 'https://dummyjson.com/auth',
     headers: {}
 })
 
-type loginData = {
-    username: string,
-    password: string,
-    expiresInMins: number
+
+
+axiosInstance.interceptors.request.use((requestObject) => {
+    if(requestObject.method?.toUpperCase() === "GET") {
+        requestObject.headers.Authorization = 'Bearer ' + getLocalStorage<IUserWithTokens>('user').accessToken
+    }
+    return requestObject
+})
+
+
+
+const login = async ({username, password, expiresInMins}: LoginData): Promise<IUserWithTokens> => {
+ const {data: userWithTokens} = await  axiosInstance.post('/login', {username, password, expiresInMins});
+ localStorage.setItem('user', JSON.stringify(userWithTokens))
+    console.log(userWithTokens)
+ return userWithTokens
 }
 
-export const login = async ({username, password, expiresInMins}: loginData): Promise<IUserWithToken> => {
-  const {data: userWithTokens} = await axiosInstance.post('/login', {username, password, expiresInMins});
-  localStorage.setItem('user', JSON.stringify(userWithTokens))
-    console.log(userWithTokens)
-   return userWithTokens
+
+
+
+const loadAuthProducts = async (): Promise<IProduct[]> => {
+const {data} = await axiosInstance.get<IProductResponseType>('/products')
+    console.log(data)
+  return data.products
 }
-// export const login = async ({username, password, expiresInMins}: loginData): Promise<IUserWithTokens> => {
-//     const {data: userWithTokens} = await  axiosInstance.post('/login', {username, password, expiresInMins});
-//     localStorage.setItem('user', JSON.stringify(userWithTokens))
-//     console.log(userWithTokens)
-//     return userWithTokens
-// }
+
+
+const refresh = async () => {
+    const iUserWithToken = getLocalStorage<IUserWithTokens>('user')
+  const {data: {accessToken, refreshToken} } =  await axiosInstance.post<ITokensPair>('refresh', {refreshToken:iUserWithToken.refreshToken, expiresInMins: 1})
+    console.log(accessToken);
+    console.log(refreshToken)
+    iUserWithToken.accessToken = accessToken;
+    iUserWithToken.refreshToken = refreshToken;
+    localStorage.setItem('user', JSON.stringify(iUserWithToken))
+}
+
+
+
+export {
+    login,
+    loadAuthProducts,
+    refresh
+}
