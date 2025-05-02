da5e33940ddbe2f5216ad86c5fcd31aa13044025
aleks1303 <aleksbulda13@gmail.com>
2025-05-02 13:07:58 +0300

    authentication

--- src/components/quotes-components/QuotesComponent.tsx
+++ src/components/quotes-components/QuotesComponent.tsx
@@ -2,12 +2,16 @@ import {useEffect, useState} from "react";
 import {loadAuthQuotes, refresh} from "../../services/api.service.tsx";
 import {IQuote} from "../../models/IQuote.ts";
 import QuoteComponent from "./QuoteComponent.tsx";
+import {useSearchParams} from "react-router-dom";
 
 
 const QuotesComponent = () => {
     const [quotes, setQuotes] = useState<IQuote[]>([])
+    const [query] = useSearchParams({page:'1'});
     useEffect(() => {
+        const page = query.get('page') || '1'
         loadAuthQuotes(page).then(response => {
+
             setQuotes(response)
         }).catch(reason => {
                 console.log(reason)
